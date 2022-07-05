d054a14fbdca948297c99a5d49a30b4bc1f512b0
Pedro Esteves <pedroesteves2803@gmail.com>
2022-07-04 21:40:29 -0300

    Alterado ate inicio das migrations

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+
+Route::get('/contato', [ContatoController ::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController ::class, 'contato'])->name('site.contato');
+
+Route::get('/login', [ContatoController ::class, 'contato'])->name('site.login');
+
+
+Route::prefix('/app')->group(function(){
+
+    Route::get('/clientes', [ContatoController ::class, 'contato'])->name('app.clientes');
+
+    Route::get('/fornecedores', [ContatoController ::class, 'contato'])->name('app.fornecedores');
+
+    Route::get('/produtos', [ContatoController ::class, 'contato'])->name('app.produtos');
 });
+
+Route::fallback(function(){
+    Echo 'rota não exite <a href="'.route('site.index').'">Clique aqui</a>';
+});
+
